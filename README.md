7ed6dd84d38bc490b95af91a771055196857187a
impossible98 <christoleavenow@outlook.com>
2024-07-31 04:40:59 +0000

    feat: add google-chrome.mdx

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Nextra Docs Template 
+# docs.impossible98.win
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b75c5670-0dee-4db5-94f2-7997cf3b45cc/deploy-status)](https://app.netlify.com/sites/roaring-pastelito-558c3a/deploys)
 
 This is a template for creating documentation with [Nextra](https://nextra.site).
 
@@ -17,7 +19,3 @@ Click the button to clone this repository and deploy it on Vercel:
 First, run `pnpm i` to install the dependencies.
 
 Then, run `pnpm dev` to start the development server and visit localhost:3000.
-
-## License
-
-This project is licensed under the MIT License.
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { useRouter } from 'next/router';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
   logo: <span>Docs</span>,
   project: {
     link: 'https://github.com/impossible98/docs.impossible98.win',
   },
-  docsRepositoryBase: 'https://github.com/impossible98/docs.impossible98.win',
+  docsRepositoryBase:
+    'https://github.com/impossible98/docs.impossible98.win/tree/master',
   footer: {
     text: 'Docs',
   },
-}
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s - Docs',
+      };
+    }
+  },
+};
 
-export default config
+export default config;
